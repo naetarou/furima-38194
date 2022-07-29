f864472637d7647fe102750c8ae0328d903f8292
naetarou <totomomo1822@gmail.com>
2022-07-30 03:42:07 +0900

    ユーザー管理機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+        
+  validates :nickname,            presence: true
+  validates :password,            presence: true, allow_blank: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, allow_blank: true }
+  validates :last_name,           presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+  validates :first_name,          presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/, allow_blank: true }
+  validates :last_name_kana,      presence: true, format: {with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+  validates :first_name_kana,     presence: true, format: {with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+  validates :birthday,            presence: true
+end
